38dfb8b26d53de8aec04445e5f8c08eeb3aaee21
Md Abrar Fahim <121944419+AbrarFahim75@users.noreply.github.com>
2026-04-10 22:54:59 +0200

    Add load_data function to load CSV files

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+def load_data(path):
+    df = pd.read_csv(path, parse_dates=["planned_date", "actual_date"])
+    return df
+
+if __name__ == "__main__":
+    df = load_data("../../data/raw/shipping_data.csv")
+    print(df.head())
