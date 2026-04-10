78857cbbde1ebfb1d5a07fe582e1095caffaa5f4
Md Abrar Fahim <121944419+AbrarFahim75@users.noreply.github.com>
2026-04-10 22:55:56 +0200

    Implement main function to load data and calculate KPIs

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+from data.load_data import load_data
+from analytics.kpi_tracker import calculate_kpis
 
+def main():
+    df = load_data("../data/raw/shipping_data.csv")
+    kpis = calculate_kpis(df)
+
+    print("📊 KPI Summary:")
+    for key, value in kpis.items():
+        print(f"{key}: {value}")
+
+if __name__ == "__main__":
+    main()
